863528818df209e86976c8155f1913ed3a163b39
Jacob Kagon <jacob@kagon.net>
2020-09-12 17:11:37 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,10 +1,9 @@
 # don't forget to add: require 'pry'
 require 'pry'
 def snake_it_up(string)
-  if string[0] == "s"
-    puts 10 * string[0] + string
+  if string[0] = "s"
+    return 10 * string[0] + string
   else
     puts string
-  puts "hello world"
   end
 end
